78c01ca93307e30172c756af13cd1b666f3c4f43
236sec <popole159za@gmail.com>
2024-09-26 15:09:11 +0700

    Add ObjectId Pipe

---
+++ app-backend/src/common/pipe.ts
@@ -0,0 +1,17 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { Types } from 'mongoose';
+import { ObjectId } from 'mongodb';
+
+@Injectable()
+export class ParseObjectIdPipe implements PipeTransform<any, Types.ObjectId> {
+  public transform(value: any): ObjectId {
+    try {
+      const transformedObjectId: ObjectId = new Types.ObjectId(
+        ObjectId.createFromHexString(value),
+      );
+      return transformedObjectId;
+    } catch (error) {
+      throw new BadRequestException('validation Failed need ObjectId');
+    }
+  }
+}
