10785c74f27fe37059c08ccc112a258bd30f60b1
236sec <popole159za@gmail.com>
2024-09-26 09:23:59 +0700

    User can See Wallet At FrontEnd

---
+++ app-frontend/app/component/WalletForm.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { Formik } from "formik";
+
+type Props = {};
+
+const WalletForm = (props: Props) => {
+  return <div>WalletForm</div>;
+};
+
+export default WalletForm;
